e1c9afa4ba7e2c6ca663295f323febb4b6be0041
Peter Kastberger <bemperum@gmail.com>
2017-03-21 21:16:09 +0100

    OneDice Helpers started
    Just a few lines of code for OneDice Character stuff.

---
+++ OneDice RPG/OneDiceRPG.psm1
@@ -0,0 +1,39 @@
+﻿<#
+.SYNOPSIS  
+    Create and manipulate a OneDice Charcter.
+      
+.DESCRIPTION  
+    Create a OneDice RPG Character and apply some methods.
+ 
+#>
+
+function New-ODChar()
+{
+    param(
+        [string]$Name,
+        [string]$Description,
+        [int]$Strong,
+        [int]$Quick,
+        [int]$Clever,
+        [int]$Magic=0
+        )
+
+    $Health = $Strong * 3
+    $Move = $Quick * 10
+    $Defence = [System.Math]::Max($Strong, $Quick) * 3
+
+    $ODChar = New-Object -TypeName PSObject -Property @{
+        Name = $Name
+        Description = $Description
+        Strong = $Strong
+        Quick = $Quick
+        Clever = $Clever
+        Magic = $Magic
+        StuntPoints = 6
+        Health = $Health
+        Move = $Move
+        Defence = $Defence
+    }
+
+    Return $ODChar
+}
---
+++ OneDice RPG/Test-OneDiceRPG.ps1
@@ -0,0 +1,20 @@
+﻿<#
+.SYNOPSIS  
+    OneDiceRPG TESTER
+      
+.DESCRIPTION  
+    Test OneDiceRPG Module
+ 
+#>
+
+
+$IMPORT_PATH = Join-Path $(Split-Path $PSCommandPath) "OneDiceRPG.psm1"
+Import-Module $IMPORT_PATH
+
+$tChar = New-ODChar -Name "Grimbart Dinglewood" `
+                    -Description "Clumsy dwarfen fighter" `
+                    -Strong 3 `
+                    -Quick 2 `
+                    -Clever 1
+
+$tChar
